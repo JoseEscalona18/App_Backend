b4c2eeae6c86acaf5284013b1e47e9db11c27051
JoseEscalona18 <114792259+JoseEscalona18@users.noreply.github.com>
2023-02-28 10:17:08 -0400

    Prueba 4
    Se agregan datos con el post efectivamente

--- routes/equipos.js
+++ routes/equipos.js
@@ -8,4 +8,16 @@ router.get('/', function(req, res, next) {
 
 });
 
+router.post('/', (req,res) => {
+
+  const { Serial , Nombre , Descripcion ,  Adquisicion , Estatus} = req.body
+  if (Serial && Nombre && Descripcion && Adquisicion && Estatus){
+    const nuevo_equipo =  {...req.body}
+    res.json('Guardado correctamente')
+    Mostrar_equipos.push(nuevo_equipo)
+  } else {
+    res.send('Peticion Erronea')
+  }
+});
+
 module.exports = router;
