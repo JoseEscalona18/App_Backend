6e7f6cc967915349b03348e9eca69667349cb29e
JoseEscalona18 <114792259+JoseEscalona18@users.noreply.github.com>
2023-02-28 09:49:29 -0400

    Cambio 3
    Se agrego el equipos.js y funciona

--- routes/equipos.js
+++ routes/equipos.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const Mostrar_equipos = require('../src/Equipos.json');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json({ title: 'Equipos' });
+  res.json(Mostrar_equipos);
+
 });
 
 module.exports = router;
