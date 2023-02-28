b06c6c343b2a7f54d3ab0a9ebd878324f441488a
Luismonsalver <luis.monri12@gmail.com>
2023-02-28 17:42:06 -0400

    Espacios
    Adición de página y visualización de espacio

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var equiposRouter = require('./routes/equipos');
+var espaciosRouter = require('./routes/espacios');
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api/equipos', equiposRouter);
+app.use('/api/espacios', espaciosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
